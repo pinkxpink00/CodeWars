83ffa8f55098a7bca5f1bba017d92098b7ac77f0
pinkxpink00 <kyperu07@gmail.com>
2023-10-14 22:18:09 +0300

    task3 8kyu

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		HoopCount(22);
+	}
+
+	static void HoopCount(int n)
+	{
+		if (n > 10)
+		{
+            Console.WriteLine("Great, now move on to tricks");
+        }
+		else
+		{
+            Console.WriteLine("Keep at it until you get it");
+        }
+	}
+}

ac11053a1ab6c3dc0b529f8ff2597e615ced6e39
pinkxpink00 <kyperu07@gmail.com>
2023-10-16 19:26:18 +0300

    task5 8kyu

---
+++ Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+
+	}
+	public static int TotalPoints(string[] games)
+	{
+		int total = 0;
+
+		foreach (string game in games)
+		{
+			if (game[0] > game[2])
+			{
+				total += 3;
+			}
+			else if (game[0] == game[2])
+			{
+				total += 1;
+			}
+
+		}
+
+		return total;
+	}
+}

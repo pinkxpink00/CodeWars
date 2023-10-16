bbb967cda807ea9497d7ce6b3b6c5dfd496ac0ca
pinkxpink00 <kyperu07@gmail.com>
2023-10-16 16:45:01 +0300

    task4 7kyu

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		UpdateLight("green");
+	}
+
+	public static string UpdateLight(string current)
+	{
+		if (current == "green")
+		{
+			return "yellow";
+		}
+		else if (current == "yellow")
+		{
+			return "red";
+		}
+		else if (current == "red")
+		{
+			return "green";
+		}
+		return "exception";
+	}
+}

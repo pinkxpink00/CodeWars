1b0ed0fe7a38bcd7cf6bfaeb9afdd9b1a768943e
pinkxpink00 <kyperu07@gmail.com>
2023-10-17 19:59:24 +0300

    Task7

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+
+	}
+
+	public static int SquareDigits(int n)
+	{
+		
+		StringBuilder squaredDigits = new StringBuilder();
+
+		
+		string numStr = n.ToString();
+
+		
+		foreach (char digitChar in numStr)
+		{
+			int digit = int.Parse(digitChar.ToString());
+			int squaredDigit = digit * digit;
+			squaredDigits.Append(squaredDigit);
+		}
+
+		
+		return int.Parse(squaredDigits.ToString());
+
+		
+	}
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine(Longest("acdvfbh", "asscvbmfmg"));
+	}
+	public static string Longest(string s1, string s2)
+	{
+		string combinate = s1 + s2;
+
+		string result = new string(combinate.Distinct().OrderBy(x => x).ToArray());
+
+		return result;
+	}
+}

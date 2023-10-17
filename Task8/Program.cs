12fbf7db290c17bccf819f7e668e7df0d859f4a7
pinkxpink00 <kyperu07@gmail.com>
2023-10-17 20:30:21 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+
+	}
+	public static string RepeatStr(int n, string s)
+	{
+		var res = "";
+		for (int i = 0; i < n; i++)
+		{
+			res += s;
+		}
+		return res;
+	}
+}

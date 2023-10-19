5a4521c5b2ed0d83c41a716c412a3bd885e8012a
pinkxpink00 <kyperu07@gmail.com>
2023-10-19 23:04:14 +0300

    task9

---
+++ Task9/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+
+	}
+
+	public static int SumMix(object[] x)
+	{
+		int sum = 0;
+
+		foreach (var element in x)
+		{
+			if (element is int)
+			{
+				sum += (int)element;
+			}
+			else if (element is string)
+			{
+				if (int.TryParse((string)element, out int num))
+				{
+					sum += num;
+				}
+			}
+		
+		}
+		return sum;
+	}
+}

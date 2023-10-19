9914c68c745995c2d2a00006655a213de6f5bfe0
pinkxpink00 <kyperu07@gmail.com>
2023-10-19 23:45:30 +0300

    task10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+
+	}
+	public static int CountSheeps(bool[] sheeps)
+	{
+		int count = 0;
+		foreach (bool b in sheeps)
+		{
+			if (b == true)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+
+}

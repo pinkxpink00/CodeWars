e21788a7fd44d1b66eaaae3ee5b8b0ef7d715433
pinkxpink00 <kyperu07@gmail.com>
2023-10-26 21:05:57 +0300

    task13

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public static string FakeBin(string x)
+    {
+        char[] res = new char[x.Length];
+
+        for (int i = 0; i < x.Length; i++)
+        {
+            if (x[i] < '5')
+            {
+                res[i] = '0';
+            }
+            else
+            {
+                res[i] = '1';
+            }
+        }
+
+        return new string(res);
+    }
+}

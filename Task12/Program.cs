9ebd254836503b7ec2026b4e76d2670a04012823
pinkxpink00 <kyperu07@gmail.com>
2023-10-26 20:54:53 +0300

    task12

---
+++ Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public static bool IsTriangle(int a, int b, int c)
+    {
+        
+        bool condition1 = a + b > c;
+        bool condition2 = a + c > b;
+        bool condition3 = b + c > a;
+
+        
+        return condition1 && condition2 && condition3;
+       
+    }
+}

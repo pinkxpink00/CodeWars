1b46c95badfcd56a0e711468ba3354a54b95b001
pinkxpink00 <kyperu07@gmail.com>
2023-11-03 16:58:32 +0200

    task15

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public static int RentalCarCost(int d)
+    {
+        int dailyRate = 40;
+        int threeDayDiscount = 20;
+        int weeklyDiscount = 50;
+
+        int calculatedCost = 0;
+
+        calculatedCost = d * dailyRate;
+
+        if ((d >= 3) && (d <= 6))
+            calculatedCost = calculatedCost - threeDayDiscount;
+
+        if (d >= 7)
+            calculatedCost = calculatedCost - weeklyDiscount;
+
+        return calculatedCost;
+    }
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsIsogram("mOose"));
+        Console.WriteLine(IsIsogram("aba"));
+        Console.WriteLine(IsIsogram("German"));
+    }
+    public static bool IsIsogram(string str)
+    {
+        HashSet<char> seenChar = new HashSet<char>();
+
+        foreach (char c in str.ToLower())
+        {
+            if (seenChar.Contains(c))
+            {
+                return false;
+            }
+
+            seenChar.Add(c);
+        }
+
+        return true;
+    }
+}

26f230a15bb5d2a4c6bae9baa859e36b0ab129a5
pinkxpink00 <kyperu07@gmail.com>
2023-11-03 17:14:52 +0200

    task16

---
+++ Task16/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Accum("abcd"));    
+        Console.WriteLine(Accum("RqaEzty")); 
+        Console.WriteLine(Accum("cwAt"));
+    }
+    public static String Accum(string s)
+    {
+        string result = "";
+
+        for(int i  = 0; i < s.Length; i++)
+        {
+            result += char.ToUpper(s[i]);
+
+            for (int j = 0; j < i; j++)
+            {
+                result += char.ToLower(s[i]);
+            }
+            
+            if (i < s.Length - 1)
+            {
+                result += "-";
+            }
+        }
+        return result;
+    }
+}

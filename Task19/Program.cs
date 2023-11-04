f61927651fdd8e6398d2a31b5506ac8b26d8c266
pinkxpink00 <kyperu07@gmail.com>
2023-11-04 21:29:57 +0200

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public static string Correct(string text)
+    {
+       
+        text = text.Replace('0', 'O').Replace('1', 'I').Replace('5', 'S');
+
+        return text;
+    }
+}

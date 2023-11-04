fac6d3e69807c598e891b04272a8d91b7f0a09d6
pinkxpink00 <kyperu07@gmail.com>
2023-11-04 21:22:56 +0200

    Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public static int Number(List<int[]> peopleList)
+    {
+        int totalPeople = 0;
+
+        foreach (var stop in peopleList)
+        {
+            totalPeople += stop[0]; // People getting on the bus
+            totalPeople -= stop[1]; // People getting off the bus
+        }
+
+        return totalPeople;
+    }
+}
